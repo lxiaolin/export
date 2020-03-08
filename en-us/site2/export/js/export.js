abc61c00b8d80ebb16222360629d3cc4df5091a9
Administrator <330805565@qq.com>
2020-03-08 12:22:52 +0800

    first commit

--- en-us/site2/export/js/export.js
+++ en-us/site2/export/js/export.js
@@ -1,9 +1,8 @@
-/*显示时间*/
-var now = new Date();
-myDate.value = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
-
-/*文件树*/
 $(function () {
+    /*显示时间*/
+    var now = new Date();
+    myDate.value = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
+
     /*文件树开关*/
     $(".main div").click(
         function () {
@@ -19,28 +18,29 @@ $(function () {
             $(this).addClass('picture1').removeClass('picture2');
         }
     });
+
+    /*上传图片*/
+    var fileDom = document.getElementById("inputLogo");
+    var previewDom = document.getElementById("preview");
+    fileDom.addEventListener("change", e => {
+        var file = fileDom.files[0];
+        if (!file || file.type.indexOf("image/") < 0) {
+            fileDom.value = "";
+            previewDom.src = "";
+            return;
+        }
+
+        var fileReader = new FileReader();
+        fileReader.onload = e => {
+            previewDom.src = e.target.result;
+        };
+        fileReader.readAsDataURL(file);
+    });
+
+    /*清除图片*/
+    $(".clearLogo").on("click", function () {
+        var blank_preview = document.getElementById("preview");
+        blank_preview.src=""
+    })
 });
 
-/*上传图片*/
-var fileDom = document.getElementById("inputLogo");
-var previewDom = document.getElementById("preview");
-fileDom.addEventListener("change", e => {
-    var file = fileDom.files[0];
-    if (!file || file.type.indexOf("image/") < 0) {
-        fileDom.value = "";
-        previewDom.src = "";
-        return;
-    }
-
-    var fileReader = new FileReader();
-    fileReader.onload = e => {
-        previewDom.src = e.target.result;
-    };
-    fileReader.readAsDataURL(file);
-});
-
-/*清除图片*/
-$(".clearLogo").on("click", function () {
-    var blank_preview = document.getElementById("preview");
-    blank_preview.src=""
-})
