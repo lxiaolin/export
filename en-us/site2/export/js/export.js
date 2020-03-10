4efc6751a666b4e2a01fe4596b691da4d1b3d0c2
Administrator <330805565@qq.com>
2020-03-10 19:03:25 +0800

    first commit

--- en-us/site2/export/js/export.js
+++ en-us/site2/export/js/export.js
@@ -1,7 +1,7 @@
 $(function () {
     /*显示时间*/
-    var now = new Date();
-    var myDate = document.getElementById("myDate")
+    let now = new Date();
+    let myDate = document.getElementById("myDate")
     myDate.value = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
 
     /*文件树开关*/
@@ -21,17 +21,17 @@ $(function () {
     });
 
     /*上传图片*/
-    var fileDom = document.getElementById("inputLogo");
-    var previewDom = document.getElementById("preview");
+    let fileDom = document.getElementById("inputLogo");
+    let previewDom = document.getElementById("preview");
     fileDom.addEventListener("change", e => {
-        var file = fileDom.files[0];
+        let file = fileDom.files[0];
         if (!file || file.type.indexOf("image/") < 0) {
             fileDom.value = "";
             previewDom.src = "";
             return;
         }
 
-        var fileReader = new FileReader();
+        let fileReader = new FileReader();
         fileReader.onload = e => {
             previewDom.src = e.target.result;
         };
@@ -40,17 +40,17 @@ $(function () {
 
     /*清除图片*/
     $(".clearLogo").on("click", function () {
-        var blank_preview = document.getElementById("preview");
+        let blank_preview = document.getElementById("preview");
         blank_preview.src = ""
     })
 });
 
 /*缩放*/
 window.onload = function () {
-    var myScale = document.getElementById('scale');
+    let myScale = document.getElementById('scale');
     myScale.onmousewheel = mouseScale;  //给div添加鼠标滚轮事件
     function mouseScale(e) {
-        var flag = true;  //设置鼠标向上还是向下滚动的变量
+        let flag = true;  //设置鼠标向上还是向下滚动的变量
         // flag = e.wheelDelta > 0 ? true : false;
         flag = e.wheelDelta > 0;
         if (flag) { //如果滚轮向上
