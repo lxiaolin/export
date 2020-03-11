fdba66442c5c608b65c7d287763acf01ffdb1d92
Administrator <330805565@qq.com>
2020-03-11 17:00:28 +0800

    first commit

--- en-us/site2/export/js/export.js
+++ en-us/site2/export/js/export.js
@@ -1,27 +1,26 @@
 $(function () {
-    /*显示时间*/
-    let now = new Date();
-    let myDate = document.getElementById("myDate")
-    myDate.value = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
+    // 显示时间
+    function updateData() {
+        let now = new Date();
+        let myDate = document.getElementById("myDate")
+        myDate.value = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
+    }
 
-    /*文件树开关*/
-    $(".main div").click(
-        function () {
-            $(this).next().slideToggle("fast");
-        }
-    );
+    // 文件树开关
+    $(".main").on("click", "div", function () {
 
-    /*图标变换*/
-    $(".picture1").on("click", function () {
-        if ($(this).hasClass('picture1')) {
-            $(this).addClass('picture2').removeClass('picture1');
+        $(this).next().slideToggle("fast");
+
+        // 图标变换
+        if ($(this).hasClass('expand')) {
+            $(this).addClass('collapse_').removeClass('expand');
         } else {
-            $(this).addClass('picture1').removeClass('picture2');
+            $(this).addClass('expand').removeClass('collapse_');
         }
     });
 
 
-    /*上传图片*/
+    /*// 上传图片
     let fileDom = document.getElementById("inputLogo");
     let previewDom = document.getElementById("preview");
     fileDom.addEventListener("change", e => {
@@ -37,45 +36,48 @@ $(function () {
             previewDom.src = e.target.result;
         };
         fileReader.readAsDataURL(file);
+        updateData();
     });
 
-    /*清除图片*/
+    // 清除图片
     $(".clearLogo").on("click", function () {
         let blank_preview = document.getElementById("preview");
         blank_preview.src = ""
+        updateData();
+    });*/
+
+    // 按钮缩放
+    let count1 = 5;
+    $(".change_up").on("click", function () {
+        if (count1 < 10) {
+            $("#right div").removeClass("change" + count1).addClass("change" + (++count1));
+        }
     });
 
-    /*按钮缩放*/
-    /*let count = 5;
-     $(".change_up").on("click", function () {
-         if (count < 10) {
-             $(".right div").removeClass("change" + count).addClass("change" + ++count);
-         }
-     });
+    $(".change_down").on("click", function () {
+        if (count1 > 0 && count1 <= 10) {
+            $("#right div").removeClass("change" + count1).addClass("change" + (--count1));
+        }
+    });
 
-     $(".change_down").on("click", function () {
-         if (count > 0 && count <= 10) {
-             $(".right div").removeClass("change" + count).addClass("change" + --count);
-         }
-     });*/
-
-    /*滚轮缩放*/
-    let count = 5;
-    $("#right").on("wheel", function (e) {
+    // 滚轮缩放
+    let count2 = 5;
+    let keyCtrlDowning = false;
+    $("#right").on("mousewheel DOMMouseScroll", function (e, delta) {
+        delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1))
         if (keyCtrlDowning) {
             e.preventDefault();
-            if (event.wheelDelta > 0) {
-                if (count < 10) {
-                    $(this).children("div").removeClass().addClass("change" + (++count));
+            if (delta > 0) {
+                if (count2 < 10) {
+                    $(this).children("div").removeClass().addClass("change" + (++count2));
                 }
-            } else if (event.wheelDelta < 0) {
-                if (count > 0 && count <= 10) {
-                    $(this).children("div").removeClass().addClass("change" + (--count));
+            } else if (delta < 0) {
+                if (count2 > 0 && count2 <= 10) {
+                    $(this).children("div").removeClass().addClass("change" + (--count2));
                 }
             }
         }
     });
-
     document.addEventListener("keydown", e => (e.key === "Control") && (keyCtrlDowning = true));
     document.addEventListener("keyup", () => keyCtrlDowning = false);
 });
---
+++ en-us/site2/export/js/export1.js
@@ -0,0 +1,187 @@
+var updateReport;
+$(() => {
+    // 后台接收数据对象
+    const data = {
+        hasLogo: true,
+        hasHeader: true,
+        header: {
+            headerAlign: "center",
+            hasTittle: true,
+            tittle: "",
+            hasSubTittle: true,
+            subTittle: "",
+            hasLogo: false,
+            imgLogo: ""
+        },
+        hasExperiment: true,
+        experiment: {
+            hasInformation: true,
+            information: {
+                name: "",
+                barcode: "",
+                userName: "",
+                instrumentName: "",
+                blockType: ""
+            },
+            hasExperimentSetting: true,
+            experimentSetting: {
+                experimentType: "",
+                chemistry: "",
+                runMode: ""
+            },
+            hasParameterSetting: true,
+            parameterSetting: {
+                reactionVolume: "",
+                lidHeatingSwitch: "",
+                coverTemp: "",
+                lidCloseTemp: "",
+            },
+            hasComments: true,
+            comments: "",
+            hasLoadPreview: true,
+            imgLoadPreviewPlate: "",
+            imgLoadPreviewProtocol: "",
+        },
+        hasPlate: true,
+        plate: {
+            hasPlateLayout: true,
+            plateLayout: [],
+            hasTask: true,
+            task: "",
+            hasTarget: true,
+            target: [],
+            hasSplitPlate: true,
+            splitPlate: "",
+            hasSample: true,
+            sample: [],
+            hasStandard: true,
+            standard: {
+                selectTarget: "",
+                standardAmount: "",
+                standardNumber: "",
+                factor: "",
+                dilutionFactor: "",
+                unit: "",
+                replicates: "",
+                increasing_decreasing: "",
+                assignment: ""
+            }
+        },
+        hasProtocol: true,
+        protocol: {
+            hasProtocol: true,
+            protocol: "",
+            advancedSetting: ""
+        },
+        hasRun: true,
+        run: {
+            hasRunTime: true,
+            runTime: "",
+            hasTemperatureCurve: true,
+            imgTemperatureCurve: ""
+        },
+        hasAnalysis: true,
+        analysis: {
+            hasQuantification: true,
+            imgAmplificationCurve_cq: "",
+            imgAmplificationCurve_log: "",
+            hasStandardCurve: true,
+            imgStandardCurve: "",
+            hasQuantificationData: true,
+            quantificationData: [],
+            hasMelt: true,
+            imgMeltCurve: "",
+            imgMeltPeak: "",
+            hasMeltData: true,
+            meltData: [],
+            hasGeneExpression: true,
+            imgGeneExpressionBar: "",
+            geneExpressionMode: "",
+            geneExpressionData: []
+        }
+    };
+
+    // 更新后台接收数据对象的方法
+    function update(toObject = data, fromObject) {
+        if (Array.isArray(toObject)) {
+            toObject.length = 0;
+            fromObject.forEach(item => toObject[toObject.length] = item);
+        } else {
+            for (let item in fromObject) {
+                if (fromObject.hasOwnProperty(item) && fromObject.hasOwnProperty(item)) {
+                    if (typeof toObject[item] === "object") {
+                        update(toObject[item], fromObject[item]);
+                    } else {
+                        toObject[item] = fromObject[item];
+                    }
+                }
+            }
+        }
+    }
+
+    // 本页面的更新主方法
+    updateReport = (obj = {}) => {
+        update(data, obj);
+    };
+
+    $("#save").on("click", () => {
+        $alertDownloading.fadeIn("fast");
+        alert("It is generating file that can be export, please wait for a few seconds.");
+        $.ajax({
+            type: "POST",
+            url: top.cqMAN.pageContextPath + "/reportAction/generate",
+            contentType: 'application/json;charset=utf-8',
+            data: JSON.stringify(data),
+            success() {
+                window.location.href = top.cqMAN.pageContextPath + "/reportAction/generateUrl";
+                alertFadeOut();
+            },
+            error() {
+                alert("A fatal unknown error has occurred.");
+                alertFadeOut();
+            }
+        });
+    });
+
+    // 新增
+
+    // 更新时间
+    function updateData() {
+        let now = new Date();
+        let myDate = document.getElementById("myDate")
+        myDate.value = now.getFullYear() + '/' + (now.getMonth() + 1) + '/' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes();
+    }
+
+
+    function updateReport1() {
+        updateData();
+        update(data, obj);
+    }
+
+    // 上传图片
+    let fileDom = document.getElementById("inputLogo");
+    let previewDom = document.getElementById("preview");
+    fileDom.addEventListener("change", e => {
+        let file = fileDom.files[0];
+        if (!file || file.type.indexOf("image/") < 0) {
+            fileDom.value = "";
+            previewDom.src = "";
+            return;
+        }
+
+        let fileReader = new FileReader();
+        fileReader.onload = e => {
+            previewDom.src = e.target.result;
+        };
+        fileReader.readAsDataURL(file);
+        updateReport1();
+    });
+
+    // 清除图片
+    $(".clearLogo").on("click", function () {
+        let blank_preview = document.getElementById("preview");
+        blank_preview.src = ""
+        updateReport1();
+    });
+
+});
